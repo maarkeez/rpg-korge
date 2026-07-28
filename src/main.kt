9eff6e7c57c7bf90cc6489b0e773bfff1720bbbb
maarkeez <d.marquez.delgado.91@gmail.com>
2026-07-28 12:33:15 +0200

    UI Skeleton

--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,4 @@
+import korlibs.encoding.Hex
 import korlibs.time.*
 import korlibs.korge.*
 import korlibs.korge.scene.*
@@ -5,11 +6,21 @@ import korlibs.korge.tween.*
 import korlibs.korge.view.*
 import korlibs.image.color.*
 import korlibs.image.format.*
+import korlibs.image.text.TextAlignment
 import korlibs.io.file.std.*
+import korlibs.korge.style.styles
+import korlibs.korge.style.textAlignment
+import korlibs.korge.style.textSize
+import korlibs.korge.ui.uiButton
+import korlibs.korge.ui.uiGridFill
+import korlibs.korge.ui.uiHorizontalStack
+import korlibs.korge.ui.uiMaterialLayer
+import korlibs.korge.ui.uiText
+import korlibs.korge.ui.uiVerticalStack
 import korlibs.math.geom.*
 import korlibs.math.interpolation.*
 
-suspend fun main() = Korge(windowSize = Size(512, 512), backgroundColor = Colors["#2b2b2b"]) {
+suspend fun main() = Korge(windowSize = Size(390, 844), backgroundColor = Colors["#2b2b2b"]) {
 	val sceneContainer = sceneContainer()
 
 	sceneContainer.changeTo { MyScene() }
@@ -17,19 +28,47 @@ suspend fun main() = Korge(windowSize = Size(512, 512), backgroundColor = Colors
 
 class MyScene : Scene() {
 	override suspend fun SContainer.sceneMain() {
-		val minDegrees = (-16).degrees
-		val maxDegrees = (+16).degrees
 
-		val image = image(resourcesVfs["korge.png"].readBitmap()) {
-			rotation = maxDegrees
-			anchor(.5, .5)
-			scale(0.8)
-			position(256, 256)
-		}
+        uiVerticalStack(padding = 2.0) {
+            uiText(
+                text = "Human turn - Round 1",
+                size = Size(width=390, height=50)
+            ){
+                this.styles.textAlignment = TextAlignment.MIDDLE_CENTER
+                this.styles.textSize = 32.0
+            }
 
-		while (true) {
-			image.tween(image::rotation[minDegrees], time = 1.seconds, easing = Easing.EASE_IN_OUT)
-			image.tween(image::rotation[maxDegrees], time = 1.seconds, easing = Easing.EASE_IN_OUT)
-		}
+            uiGridFill(
+                size= Size(width=390, height=390),
+                cols = 8,
+                rows = 8) {
+
+                for (n in 0 until cols * rows) {
+                    uiButton("$n").also { button ->
+                        button.bgColorOut = RGBA(229, 229, 229, 0)
+                        button.bgColorOver = RGBA(252, 252, 252, 0)
+                        button.background.borderColor = RGBA(187, 187, 187, 0)
+                    }
+                }
+            }
+
+            uiHorizontalStack(padding = 1.0) {
+                uiMaterialLayer().also {
+                    it.size = Size(width= 195, height=200)
+                    it.radius = RectCorners(16f,16f,16f, 8f)
+                    it.bgColor = Colors.SKYBLUE
+                    it.borderColor = Colors.DIMGREY
+                    it.borderSize = 2.0
+                }
+                uiMaterialLayer().also {
+                    it.size = Size(width= 195, height=200)
+                    it.radius = RectCorners(16f,16f,16f, 8f)
+                    it.bgColor = Colors.SKYBLUE
+                    it.borderColor = Colors.DIMGREY
+                    it.borderSize = 2.0
+                }
+            }
+        }
+
 	}
 }
