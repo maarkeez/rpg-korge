dba01abf9226887550c21637be927309ac1dad50
David Márquez Delgado <d.marquez.delgado.91@gmail.com>
2026-08-06 17:32:20 +0200

    Comments

--- src/battlefield/adapters/presentation/BattlefieldApi.kt
+++ src/battlefield/adapters/presentation/BattlefieldApi.kt
@@ -8,9 +8,14 @@ import battlefield.usecases.queries.SearchBattlefield
 import shared.domain.EventBus
 
 class BattlefieldApi(eventBus: EventBus) {
+    // Storage
     private val battlefieldRepository = InMemoryBattlefieldRepository()
+
+    // Commands
     val initializeBattlefield = InitializeBattlefield(battlefieldRepository,eventBus)
     val removeOccupant = RemoveOccupant(battlefieldRepository, eventBus)
     val updateBattlefieldOccupancy = UpdateBattlefieldOccupancy(battlefieldRepository, eventBus)
+
+    // Queries
     val searchBattlefield = SearchBattlefield(battlefieldRepository)
 }
