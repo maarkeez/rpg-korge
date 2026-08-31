f88fbb5bd59e7f0af924fb473d20d8b77176d534
maarkeez <d.marquez.delgado.91@gmail.com>
2026-08-31 22:18:03 +0200

    Fixed bug: cpu brain trying to move defeated battle units

--- src/battleunit/usecases/queries/SearchBattleUnitsByPlayerId.kt
+++ src/battleunit/usecases/queries/SearchBattleUnitsByPlayerId.kt
@@ -5,5 +5,7 @@ import battleunit.domain.*
 class SearchBattleUnitsByPlayerId(
     private val battleUnitRepository: BattleUnitRepository
 ) {
-    operator fun invoke(playerId: String): List<BattleUnit.Dto> = battleUnitRepository.searchByPlayerId(playerId).map { it.toDto() }
+    operator fun invoke(playerId: String): List<BattleUnit.Dto> = battleUnitRepository.searchByPlayerId(playerId)
+        .filter { !it.isDefeated() }
+        .map { it.toDto() }
 }
