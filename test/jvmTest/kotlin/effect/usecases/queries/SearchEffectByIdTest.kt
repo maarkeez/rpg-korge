dd27b4b10d08a6999592372b2bd4a6c11fce8869
maarkeez <d.marquez.delgado.91@gmail.com>
2026-09-06 09:56:11 +0200

    Add SearchEffectByIdTest

---
+++ test/jvmTest/kotlin/effect/usecases/queries/SearchEffectByIdTest.kt
@@ -0,0 +1,34 @@
+package effect.usecases.queries
+
+import effect.adapters.storage.*
+import effect.domain.EffectMother
+import effect.domain.EffectMother.effect
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.*
+
+class SearchEffectByIdTest {
+    private val effectRepository = InMemoryEffectRepository()
+    private val searchEffectById = SearchEffectById(effectRepository)
+
+    @Test
+    fun `should return effect when it exists`() {
+        // Given
+        val effect = effect()
+        val effectDto = effect.toDto()
+        effectRepository.create(effect)
+        // When
+        val storedEffect = searchEffectById(effectDto.id)
+        // Then
+        assertThat(storedEffect).isEqualTo(effectDto)
+    }
+
+    @Test
+    fun `should return null when effect does not exist`() {
+        // Given
+        val unknownEffectId = EffectMother.effectId()
+        // When
+        val storedEffect = searchEffectById(unknownEffectId)
+        // Then
+        assertThat(storedEffect).isNull()
+    }
+}
