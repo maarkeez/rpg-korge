d9d3ef6587fc493de6d2c3daf182012e9e5d1906
maarkeez <d.marquez.delgado.91@gmail.com>
2026-09-08 19:30:38 +0200

    Update JUnit to 6.1.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,9 +31,9 @@ dependencies {
     add("commonMainApi", project(":deps"))
     add("jvmTestApi", "org.mockito.kotlin:mockito-kotlin:6.3.0")
     add("jvmTestApi", "org.assertj:assertj-core:3.27.7")
-    add("jvmTestApi", "org.junit.jupiter:junit-jupiter:5.10.2")
+    add("jvmTestApi", "org.junit.jupiter:junit-jupiter:6.1.3")
     add("jvmTestApi", "junit:junit:4.13.2")
-    add("jvmTestRuntimeOnly", "org.junit.vintage:junit-vintage-engine:5.10.2")
+    add("jvmTestRuntimeOnly", "org.junit.vintage:junit-vintage-engine:6.1.3")
 }
 
 tasks.withType<Test>().configureEach {
