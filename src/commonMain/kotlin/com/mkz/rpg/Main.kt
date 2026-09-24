750fb6c8096759d9f26ecc51f604cd00c526921f
David Márquez Delgado <d.marquez.delgado.91@gmail.com>
2026-09-24 18:23:16 +0200

    Modified main to use battle scene

--- src/commonMain/kotlin/com/mkz/rpg/Main.kt
+++ src/commonMain/kotlin/com/mkz/rpg/Main.kt
@@ -1,20 +1,20 @@
 package com.mkz.rpg
 
-import com.mkz.rpg.levelDesign.screen.LevelDesignColors
-import com.mkz.rpg.levelDesign.screen.LevelDesignScene
+import com.mkz.rpg.screen.BattleScene
+import korlibs.image.color.Colors
 import korlibs.korge.Korge
 import korlibs.korge.scene.sceneContainer
 import korlibs.math.geom.Size
 
 suspend fun main() =
-//    Korge(windowSize = Size(390, 844), backgroundColor = Colors["#2b2b2b"]) {
-//        val sceneContainer = sceneContainer()
-//
-//        sceneContainer.changeTo { BattleScene() }
-//    }
-
-    Korge(windowSize = Size(width = 2560, height = 1440), backgroundColor = LevelDesignColors.BLACK) {
+    Korge(windowSize = Size(390, 844), backgroundColor = Colors["#2b2b2b"]) {
         val sceneContainer = sceneContainer()
 
-        sceneContainer.changeTo { LevelDesignScene() }
+        sceneContainer.changeTo { BattleScene() }
     }
+
+// Korge(windowSize = Size(width = 2560, height = 1440), backgroundColor = LevelDesignColors.BLACK) {
+//    val sceneContainer = sceneContainer()
+//
+//    sceneContainer.changeTo { LevelDesignScene() }
+// }
